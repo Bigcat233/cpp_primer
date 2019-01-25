a3e2faeb35562aaded0dbf01697415bf9fec2ec6
Rico-nan <jn0792@qq.com>
2019-01-25 23:44:18 +0800

    error sales_item

--- chapter_1/pra/1/main.cpp
+++ chapter_1/pra/1/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
+#include "Sales_item.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Sales_item book;
+    //test
+    cin>>book;
+    cout<<book<<endl;
     return 0;
 }
