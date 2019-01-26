cb3edc8676d1bf1d87b1df4bc202756fd8198983
Rico-nan <jn0792@qq.com>
2019-01-26 23:52:33 +0800

    empty

---
+++ chapter_2/pra2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //double Double; //c++区分大小写，并且很简单的办法是打上去后Double并没有变色
+    //the difference between
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
