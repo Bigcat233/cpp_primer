3e4601680f20e8a3d6aa7a58490033b19e1fbc4b
Rico-nan <jn0792@qq.com>
2019-01-27 14:44:49 +0800

    finish chapter2

--- chapter_2/pra2/main.cpp
+++ chapter_2/pra2/main.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main()
 {
     //double Double; //c++区分大小写，并且很简单的办法是打上去后Double并没有变色
-    //the difference between
-
+    //the difference between NULL and nullptr：NULL其实就是0，而nullptr表示空指针，在新标准下，最好使用nullptr 避免NULL的使用
+    /*const int const_nm = 1;
+    const_nm=2;*/ //出错因为const型变量只读
     cout << "Hello world!" << endl;
     return 0;
 }
