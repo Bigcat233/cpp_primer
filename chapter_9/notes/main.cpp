bef54e938d515c10e0d666330ece2038ab0f5e32
Rico-nan <jn0792@qq.com>
2019-01-28 20:03:46 +0800

    open chapter_8

---
+++ chapter_9/notes/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+//    几种顺序容器的区分与总结
+    //string vector都是连续存储空间
+    //vector deque list forward_list array string
+    //对于一般选择使用哪个容器使用vector是最好的选择，除非有很好的理由选择其他容器
+    vector<vector<string>> lines;
+    return 0;
+}
