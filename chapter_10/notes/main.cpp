967824130269ada2c977e0361c7fdc52c8cfdda6
Rico-nan <jn0792@qq.com>
2019-01-31 18:47:04 +0800

    update algorithm

--- chapter_10/notes/main.cpp
+++ chapter_10/notes/main.cpp
@@ -6,6 +6,9 @@
 #include <numeric>
 using namespace std;
 
+bool is_div_five(int i){
+        return i%5?0:1;
+    }
 int main()
 {
     //generic algorithm
@@ -29,15 +32,58 @@ int main()
     //写算法
     //需要时刻注意的是 算法始终不会执行容器操作，不会改变容器大小
     //删除重复元素
-    vector<string> t{"hello","the","fdsfds","fff","the","tyu","yui","yui","a"};
-    sort(t.begin(),t.end());
-    for(auto c:t){
-        cout<<c<<endl;
+    vector<string> str{"hello","the","fdsfds","fff","the","tyu","yui","yui","a"};
+    vector<int> number;
+    for(int i=0;i!=100;i++){
+        number.push_back(i);
     }
-    auto end_unique = unique(t.begin(),t.end());
-    t.erase(end_unique,t.end());
-    for(auto c:t){
-        cout<<c<<endl;
-    }
+//    sort(t.begin(),t.end());
+//    for(auto c:t){
+//        cout<<c<<endl;
+//    }
+//    auto end_unique = unique(t.begin(),t.end());
+//    t.erase(end_unique,t.end());
+//    for(auto c:t){
+//        cout<<c<<endl;
+//    }
+
+    //标准库的一些常用算法
+    //查找对象：find
+    //find_if,find_if_not,count,count_if
+    //find_if template <class InputIterator, class UnaryPredicate>
+   //InputIterator find_if (InputIterator first, InputIterator last, UnaryPredicate pred);
+    //Find element in range
+
+
+//    vector<int>::const_iterator location;
+//    location=find_if(number.cbegin(),number.cend(),is_div_five);
+//    if(location!=number.cend()){
+//        cout<<"the first number can divided by 5\n"<<*location<<endl;
+//    }
+    //find_if_not同理
+
+
+    //count
+//    int a =count(number.cbegin(),number.cend(),5);
+//    cout<<"tongji"<<a;
+
+    //count_if
+//    a = count_if(number.cbegin(),number.cend(),is_div_five);
+//    cout<<"tongji"<<a;
+//
+    //查找重复元素 函数第三个参数还可以自定义重复的含义，
+//    auto location = adjacent_find(str.cbegin(),str.cend());
+//    cout<<*location<<endl;
+
+
+
     return 0;
 }
+
+
+
+
+
+
+
+
